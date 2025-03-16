a629c2ce4326e60807923eba77d308c0d7bcb447
kabali <kabalivenu8368@gmail.com>
2025-03-16 15:23:31 +0530

    For Jenkins Purpose

--- src/test/java/demoPackage/Demo1_1.java
+++ src/test/java/demoPackage/Demo1_1.java
@@ -7,6 +7,7 @@ public class Demo1_1 {
 		System.out.println("Hii Demo1_1");
 		System.out.println("Hii Extra Demo1_1");
 		System.out.println("Hii Demo confusion one for jenkins");
+		System.out.println("For Jenkins purpose");
 
 	}
 
